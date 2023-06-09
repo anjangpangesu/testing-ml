ed0c88d4762be61a3d3e9d9fa8db6cb12d27e7e5
anjangpangesu <anjangpangestu2304@gmail.com>
2023-06-09 20:06:36 +0700

    Full Edit

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 from fastapi import FastAPI
-import tensorflow as tf
 from pydantic import BaseModel
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import load_model
 
-
-# Inisialisasi FastAPI
-app = FastAPI()
-
-# Definisikan struktur data input menggunakan Pydantic BaseModel
+# Define the input data schema
 
 
 class InputData(BaseModel):
-    age: int
+    age: float
     sex: int
     rbc: float
     hgb: float
@@ -26,24 +24,45 @@ class InputData(BaseModel):
     eos: float
     ba: float
 
+# Define the output data schema
 
-# Muat model dari file H5
-model = tf.keras.models.load_model('./elaborate_model.h5')
+
+class OutputData(BaseModel):
+    prediction: int
+
+
+# Load the ML model
+model = load_model('./elaborate_model.h5')
+
+# Initialize the FastAPI app
+app = FastAPI()
+
+# Define the get endpoint
 
 
 @app.get("/")
 def hello():
-    return {"message": "fastAPI CloudRun Tensorflow Deployment"}
-
-# Definisikan endpoint untuk prediksi
+    return {"message": "ML Model Success to Deploy"}
 
 
-@app.post('/predict')
+# Define the prediction endpoint
+
+
+@app.post("/predict", response_model=OutputData)
 def predict(data: InputData):
-    # Lakukan prediksi menggunakan model
-    predict = model.predict([[data.age, data.sex, data.rbc, data.hgb, data.hct, data.mcv, data.mch, data.mchc,
-                              data.rdw_cv, data.wbc, data.neu, data.lym, data.mo, data.eos, data.ba]])
+    # Convert input data to a numpy array
+    input_array = np.array([[
+        data.age, data.sex, data.rbc, data.hgb, data.hct, data.mcv, data.mch,
+        data.mchc, data.rdw_cv, data.wbc, data.neu, data.lym, data.mo, data.eos, data.ba
+    ]])
 
-    # Kembalikan hasil prediksi
-    res = predict.item()
-    return {"res": res}
+    # Perform the prediction
+    prediction = model.predict(input_array)
+
+    # Convert the prediction to the corresponding class label
+    class_label = np.argmax(prediction, axis=1)[0]
+
+    # Create the output data
+    output_data = OutputData(prediction=class_label)
+
+    return output_data
