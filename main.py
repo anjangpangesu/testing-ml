534cab421660d431694c123f928c3ea37b82e642
anjangpangesu <anjangpangestu2304@gmail.com>
2023-06-11 23:44:56 +0700

    Update All

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import tensorflow as tf
+from tensorflow.keras.models import load_model
 import numpy as np
-import pandas as pd
-from sklearn.preprocessing import StandardScaler
+
+app = FastAPI()
+
+# Load the model
+model = load_model('./model.h5')
 
 
-# Define the input schema
+# Define the request model
 class InputData(BaseModel):
     age: float
-    sex: float
+    sex: int
     rbc: float
     hgb: float
     hct: float
@@ -25,12 +29,10 @@ class InputData(BaseModel):
     ba: float
 
 
-# Load the saved model
-model = tf.keras.models.load_model('./model.h5')
-
-
-# Create the FastAPI app
-app = FastAPI()
+# Define the response model
+class Prediction(BaseModel):
+    interpretation: str
+    class_: int
 
 
 # Define the default route
@@ -39,25 +41,23 @@ def hello():
     return {"message": "fastAPI CloudRun Tensorflow Deployment"}
 
 
-# Define the prediction route
-@app.post("/predict")
-def predict(data: InputData):
-    # Convert the input data to a DataFrame
-    input_df = pd.DataFrame([data.dict()])
+# Endpoint for model prediction
+@app.post("/predict", response_model=Prediction)
+def predict(input_data: InputData):
+    # Convert input data to a NumPy array
+    input_array = np.array([[input_data.age, input_data.sex, input_data.rbc, input_data.hgb, input_data.hct,
+                            input_data.mcv, input_data.mch, input_data.mchc, input_data.rdw_cv, input_data.wbc,
+                            input_data.neu, input_data.lym, input_data.mo, input_data.eos, input_data.ba]])
 
-    # Standardize the input features
-    scaler = StandardScaler()
-    input_scaled = scaler.fit_transform(input_df)
+    # Perform prediction using the model
+    predictions = model.predict(input_array)
 
-    # Make predictions
-    predictions = model.predict(input_scaled)
+    # Decode the predictions
+    interpretation_idx = np.argmax(predictions[:, :2])
+    class_idx = np.argmax(predictions[:, 2:])
+    interpretation = interpretation_idx
+    class_ = class_idx
 
-    # Prepare the response
-    interpretation = np.argmax(predictions[0][:2])
-    class_ = np.argmax(predictions[0][2:])
-    response = {
-        "interpretation": interpretation,
-        "class": class_
-    }
-
+    # Create the response
+    response = Prediction(interpretation=interpretation, class_=class_)
     return response
