be881870c061cdc43d5f11d8ce3e0c514f814b11
anjangpangesu <anjangpangestu2304@gmail.com>
2023-06-15 15:28:44 +0700

    First Commit

--- main.py
+++ main.py
@@ -1,95 +1,61 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import numpy as np
-import tensorflow as tf
 from tensorflow.keras.models import load_model
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
+from sklearn.preprocessing import StandardScaler
 
-# Initialize Firebase credentials and Firestore
-cred = credentials.Certificate("./serviceAccountKey.json")
-firebase_admin.initialize_app(cred)
-db = firestore.client()
+app = FastAPI()
 
-# Define the input data schema
+# Define the input data structure
 
 
-class InputData(BaseModel):
-    age: int
+class PredictionRequest(BaseModel):
+    age: float
     sex: int
     rbc: float
-    hgb: float
+    rbc: float
     hct: float
     mcv: float
     mch: float
     mchc: float
     rdw_cv: float
     wbc: float
-    neu: float
-    lym: float
+    wbc: float
+    wbc: float
     mo: float
     eos: float
     ba: float
 
-# Define the output data schema
+
+# Load the trained model
+model = load_model('elaborate_model.h5')
+scaler = StandardScaler()
+
+# Define the prediction route
 
 
-class OutputData(BaseModel):
-    prediction: int
+@app.post("/predict")
+def predict(request: PredictionRequest):
+    # Prepare the input data
+    input_data = np.array(list(request.dict().values())).astype(np.float32)
+    # Scale the input using the same scaler used during training
+    input_data = scaler.transform([input_data])
 
+    # Make the prediction
+    prediction = model.predict(input_data)
+    predicted_class = np.argmax(prediction)
 
-# Load the ML model
-model = load_model('./elaborate_model.h5')
-
-# Initialize the FastAPI app
-app = FastAPI()
-
-# Define the GET endpoint to retrieve data
-
-
-@app.get("/data/{data_id}")
-def get_data(data_id: str):
-    doc_ref = db.collection('data').document(data_id)
-    doc = doc_ref.get()
-    if doc.exists:
-        return doc.to_dict()
-    else:
-        return {"message": "Data not found"}
-
-# Define the GET endpoint for hello message
-
-
-@app.get("/")
-def hello():
-    return {"message": "ML Model Success to Deploy"}
-
-# Define the prediction endpoint
-
-
-@app.post("/predict", response_model=OutputData)
-def predict(data: InputData):
-    # Convert input data to a numpy array
-    input_array = np.array([[
-        data.age, data.sex, data.rbc, data.hgb, data.hct, data.mcv, data.mch,
-        data.mchc, data.rdw_cv, data.wbc, data.neu, data.lym, data.mo, data.eos, data.ba
-    ]])
-
-    # Perform the prediction
-    prediction = model.predict(input_array)
-
-    # Convert the prediction to the corresponding class label
-    class_label = np.argmax(prediction, axis=1)[0]
-
-    # Create the output data
-    output_data = OutputData(prediction=class_label)
-
-    # Save the input and output data to Firestore
-    data_dict = {
-        'input_data': data.dict(),
-        'output_data': output_data.dict()
+    # Define the class labels
+    class_labels = {
+        0: "Normal health status: no immediate need for medical consultation",
+        1: "It is advisable to seek a medical consultation in order to facilitate a comprehensive assessment for further diagnostic purposes.",
+        2: "Medical consultation recommended for moderate condition",
+        3: "Immediate medical consultation required"
     }
-    doc_ref = db.collection('data').document()
-    doc_ref.set(data_dict)
 
-    return output_data
+    # Create the response
+    response = {
+        "prediction": class_labels[predicted_class]
+    }
+
+    return response
